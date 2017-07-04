19f8ffa3266648341361c1b35d1572945bf93783
Kayhan <kdehghan@sfu.ca>
2017-07-04 09:17:54 -0700

    Clean weather data. Drop nan values in weather column. Store only Date/Time and Weather column.

--- clean_data.py
+++ clean_data.py
@@ -1,24 +1,38 @@
-import sys
-import numpy as np
 import pandas as pd
+from os import listdir
+from os.path import isfile, join
 
-path = "../yvr-weather/"
+src_path = "../yvr-weather/"
+dest_path = "../cleaned-weather-data/"
+
 
 def read_ghcn_data(filename):
     """ Return the data from text and csv files """
-    # read csv file
-    ghcn_data = pd.read_csv(filename, header=None)
+    # only read Date/Time (as index) and Weather columns.
+    ghcn_data = pd.read_csv(filename, usecols=[0, 24], index_col='Date/Time')
     return ghcn_data
 
-def output_csv(data):
+
+def output_csv(data, filename):
     """ Write the data to a CSV file """
-    data.to_csv(out_csv, sep='\t')
+    data.to_csv(dest_path + filename, sep='\t')
+
+
+def get_all_csv_filenames():
+    image_files = [f for f in listdir(src_path) if isfile(join(src_path, f))]
+    return image_files
 
 
 def main():
     """ Main function """
-    ghcn_data = read_ghcn_data(path + "201606.csv")
-    print (ghcn_data)
+    csv_filenames = get_all_csv_filenames()
+
+    for filename in csv_filenames:
+        if filename[len(filename) - 3:] == 'csv':
+            ghcn_data = read_ghcn_data(src_path + filename)
+            # drop NaN values
+            ghcn_data = ghcn_data.dropna()
+            output_csv(ghcn_data, filename)
 
 if __name__ == '__main__':
     main()
